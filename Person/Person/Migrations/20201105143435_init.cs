37f7781286954244e5d1b3092b4b425d947dc949
Tran Duy Kien <tranduykien1997@gmail.com>
2020-11-05 21:40:28 +0700

    update codefirst Person

---
+++ Person/Person/Migrations/20201105143435_init.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Person.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StaffId = table.Column<string>(nullable: true),
+                    FullName = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Location = table.Column<int>(nullable: false),
+                    Avatar = table.Column<string>(nullable: true),
+                    Decription = table.Column<string>(nullable: true),
+                    Phone = table.Column<string>(nullable: true),
+                    YearOfBirth = table.Column<DateTime>(nullable: false),
+                    Gender = table.Column<int>(nullable: false),
+                    CreatedBy = table.Column<string>(nullable: true),
+                    UpdatedBy = table.Column<string>(nullable: true),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdateAt = table.Column<DateTime>(nullable: false),
+                    Status = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Persons");
+        }
+    }
+}
---
+++ Person/Person/Models/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Person.Models
+{
+    public class Person
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string StaffId { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public int Location { get; set; }
+        public string Avatar { get; set; }
+        public string Decription { get; set; }
+        public string Phone { get; set; }
+        public DateTime YearOfBirth { get; set; }
+        public int Gender { get; set; }
+        public string CreatedBy { get; set; }
+        public string UpdatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdateAt { get; set; }
+        public bool Status { get; set; }
+    }
+}
